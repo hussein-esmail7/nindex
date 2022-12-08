663dc402dd1ed7bc1e0b50fd0835bcd1f22e282b
Hussein Esmail <hussein.esmail7@gmail.com>
2022-12-07 21:46:39 -0500

    Would you prefer a commit message and no code or code and no commit message?

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # notebook-index
-A program to quickly find a scanned page from a given index and open it if it exists
+A program to quickly find a scanned page from a given index and open it if it exists.
+
+## Table of Contents
+- [What is this?](#what-is-this)
+- [Requirements](#requirements)
+- [Installation](#installation)
+    - [Installing Through Git](#installing-through-git)
+- [Running the Program](#running-the-program)
+- [Arguments](#arguments)
+- [Donate](#donate)
+
+## What is this?
+
+## Requirements
+- fzf
+
+## Installation
+At the moment, you can only `git clone` this repository, but I am hoping to put
+it on Homebrew/AUR soon.
+
+## Installing Through Git
+```
+git clone https://github.com/hussein-esmail7/notebook-index
+cd notebook-index
+```
+If you want to run this program in Terminal just by typing the program name,
+you would have to add an alias in your `.bashrc` file. For example, if you want it to run when you type just `n`, you would put this line in your `.bashrc`:
+```
+alias n='path/to/folder/notebook-index/notebook-index.sh
+```
+
+## Running the program
+To use this program, you have to make sure you are in the correct directory,
+and that the file has executable permission.
+```
+cd notebook-index
+chmod +x notebook-index.sh
+```
+
+Personally, I assigned the file `notebook-index.sh` as an alias in my `.bashrc`
+so whichever directory I'm in, I can just type `n <args>` and it will run the program in whatever directory I'm in.
+
+## Arguments
+> **Note**
+> For this section, I'm assuming you have read [How I Format My Notebooks](#how-i-format-my-notebooks).
+When running this program, you can type in the notebook number
+
+
+## Donate
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/husseinesmail)
---
+++ notebook-index.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# notebookpg.sh
+# Hussein Esmail
+# Created: 2022 10 26
+# Description: This program will use fzf to open my notebook pages
+
+
+FOLDER_NOTEBOOKS="$HOME/Documents/Notebooks/*"
+INDEX_FILE="$HOME/Documents/Notebooks/N-Indexes.tex"
+files=("$FOLDER_NOTEBOOKS"*) # Get list of all notebooks in the folder
+CONFIG_QUIET=1 # Quiet mode by default. Pass "-v" for verbose
+CONFIG_SCAN=0 # Do not rescan files unless told
+FILE_FZFNAMES="$HOME/git/notebook-index/notebookcache.txt"
+FZF_PROMPT="Notebook Search: "
+
+# Checking for command arguments
+while getopts ":vsh:" opt; do
+	case $opt in
+		v)
+			echo "ERROR: '-v' option not implemented yet!"
+			# TODO
+			shift
+			;;
+		s)
+			CONFIG_SCAN=1 # Rescan files before showing the fzf menu
+			shift
+			;;
+		h)  # Help message
+			echo "usage: n-i"
+			echo ""
+			echo "This program quickly opens a specific notebook page"
+			echo ""
+			echo "Requirements: fzf"
+			echo ""
+			echo "See https://github.com/hussein-esmail7/notebook-index for more info"
+			exit 1
+			;;
+		\?)
+			echo "ERROR: '$opt' is an invalid option!"
+			exit 1
+			;;
+	esac
+done
+
+if [ ! -f "$FILE_FZFNAMES" ] ; then
+	# Notebook cache file does not exist, '-s' needs to be run
+	CONFIG_SCAN=1
+	echo "Warning: Running with scan option '-s'"
+	echo "         since $FZF_FILENAMES does not exist"
+fi
+
+
+if [ $CONFIG_SCAN -eq 1 ] ; then
+	iFile=$(cat "$INDEX_FILE") # Read the LaTeX index file contents
+	SAVEIFS=$IFS
+	IFS=$(echo -en "\n\b")
+	pageTitles=() # Initialize array where page titles will be stored
+	nREGEX="N[0-9]{2}-P[0-9]{3}.*+$"
+	for nFolder in $FOLDER_NOTEBOOKS ; do
+		# For every notebook folder
+		if [ $CONFIG_QUIET -eq 0 ] ; then
+			echo "$nFolder" # Print the file/folder name
+		fi
+		if [ -d "$nFolder" ] ; then # If the file/folder is a folder
+			cd "$nFolder" # Change directories to that folder
+			for nPageFile in * ; do # For every file in that folder (n-page)
+				nPage=${nPageFile%".jpg"} # Remove the .jpg suffix
+				pTitle=$(grep $nPage $INDEX_FILE | xargs) # Find that .tex entry
+				pTitle=${pTitle#"\\"}				# Remove LaTeX formatting 1
+				pTitle=${pTitle#"item[$nPage] "}	# Remove LaTeX formatting 2
+				pageAppend="$nPage | $pTitle"
+				if [ $CONFIG_QUIET -eq 0 ] ; then
+					echo "     $pageAppend"
+				fi
+				pageTitles+=("$pageAppend") # Add formatted line to final array
+			done
+		fi
+	done
+	IFS=$SAVEIFS
+	# touch "$FILE_FZFNAMES"
+	# echo "File should exist now"
+	# for i in "${pageTitles[@]}" ; do
+	# 	echo "$i" >> "$FILE_FZFNAMES"
+	# done
+	printf "%s\n"  "${pageTitles[@]}" > "$FILE_FZFNAMES" # Write lines to file
+fi
+
+
+if [ $CONFIG_QUIET -eq 0 ] ; then
+	cat "$FILE_FZFNAMES"
+fi
+
+cat "$FILE_FZFNAMES" | fzf --tac --prompt="$FZF_PROMPT" # Feed lines to fzf
+# --tac		--> Reverse the order of the output
+# --prompt	--> Change the prompt line while in fzf
+
+exit 0
