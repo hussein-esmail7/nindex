ef9d0c95d8d1de1460962546b0b02e69f2af3646
Hussein Esmail <hussein.esmail7@gmail.com>
2024-04-05 11:51:50 -0400

    Search a specific notebook only

--- nindex.sh
+++ nindex.sh
@@ -39,6 +39,23 @@ if [[ "$1" =~ [0-9]{1,2}$ && "$2" =~ [0-9]{1,3}$ ]] ; then
  	tmp_p_num=$(echo "$args" | awk '{print $2}')
  	tmp_p_num=$(printf "%03d\n" $tmp_p_num)
  	P_CHOICE="N${tmp_n_num}-P${tmp_p_num}"
+elif [[ "$1" =~ [n|N][0-9]{1,2}$ && -z "$2" ]] ; then
+	# Cases: "N09", "n12", "n1"
+ 	# When only one number is given with a prefix "n" (or "N"), indicates the notebook number
+ 	# Run fzf but only with the pages of that notebook
+	grep_str=$(echo "$1" | tr "n" "N") # Make "n" capital if needed. So we can grep the index file (case-sensitive)
+	if [[ "${#grep_str}" -eq 2 ]] ; then
+		# When the string is 2 characters long, put a 0 in between
+		# Ex. "N1" to "N01"
+		grep_str="${grep_str:0:1}"0"${grep_str:1}"
+	fi
+	# echo "Searching $grep_str only..."
+	IFS=$'\n';
+	cd "$( dirname "${BASH_SOURCE[0]}" )" ; "$C_FILE_PATH" "$NOTEBOOK_FOLDER" "$NOTEBOOK_INDEX" | grep "$grep_str" | sort > "$FILE_LIST"
+
+	# TODO: Pass all unprocessed arguments to fzf as a query
+	P_CHOICE=$(cat "$FILE_LIST" | fzf --tac --prompt="$FZF_PROMPT" | cut -d " " -f1)
+
 elif [[ "$1" =~ [0-9]{1,3}$ && -z "$2" ]] ; then
 	# Cases: "09", "123", "12"
  	# When only one number is given, which indicates the page number.
@@ -68,7 +85,7 @@ elif [[ "$1" == "-h" || "$1" == "help" ]] ; then
 	exit 1
 elif [[ "$1" == "-e" || "$1" == "edit" ]] ; then
 	# Edit the index file
-	$EDITOR "$NOTEBOOK_INDEX" 
+	$EDITOR "$NOTEBOOK_INDEX"
 	exit 1
 elif [[ "$1" == "-l" || "$1" == "list" ]] ; then
 	# Print the index, then exit
@@ -77,28 +94,36 @@ elif [[ "$1" == "-l" || "$1" == "list" ]] ; then
 else # Run as normal
 	IFS=$'\n';
 	cd "$( dirname "${BASH_SOURCE[0]}" )" ; "$C_FILE_PATH" "$NOTEBOOK_FOLDER" "$NOTEBOOK_INDEX" | sort > "$FILE_LIST"
+
+	# TODO: Pass all unprocessed arguments to fzf as a query
 	P_CHOICE=$(cat "$FILE_LIST" | fzf --tac --prompt="$FZF_PROMPT" | cut -d " " -f1)
 fi
 
 # [ ! -z "$P_CHOICE" ] && echo "PCHOICE: $P_CHOICE"
 rm -f "$FILE_LIST" # Delete temporary cache file
 
-# Find the chosen file
-FILE_LOCATION=$(find "$NOTEBOOK_FOLDER" -name "$P_CHOICE*")
-# Explanation for "$P_CHOICE*" part:
-#   At first, I had "$P_CHOICE.jpg", but this does not find the pages that
-#   may have " - rescan" in the file name. "$P_CHOICE" does not return
-#   anything because it thinks that's the whole file name, so I need the
-#   wildcard
+if [[ "$P_CHOICE" != *"\n"* ]] ; then
+	# If no option is given in fzf, it may return the whole list. This can be
+	# caught by seeing if $P_CHOICE is multiple lines
 
-# Open the file
-if [ -z "$FILE_LOCATION" ] ; then # Check if file exists first
-	echo "$P_CHOICE does not exist!"
-	exit 1
-elif [[ $OSTYPE == "darwin"* ]] ; then # macOS
-	open "$FILE_LOCATION"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
-	okular "$FILE_LOCATION" & > /dev/null
+	# Find the chosen file
+	FILE_LOCATION=$(find "$NOTEBOOK_FOLDER" -name "$P_CHOICE*")
+	# Explanation for "$P_CHOICE*" part:
+	#   At first, I had "$P_CHOICE.jpg", but this does not find the pages that
+	#   may have " - rescan" in the file name. "$P_CHOICE" does not return
+	#   anything because it thinks that's the whole file name, so I need the
+	#   wildcard
+
+	# Open the file
+
+	if [[ ! -f "$FILE_LOCATION" ]] ; then # Check if file exists first
+		echo "File does not exist!"
+		exit 1
+	elif [[ $OSTYPE == "darwin"* ]] ; then # macOS
+		open "$FILE_LOCATION"
+	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
+		okular "$FILE_LOCATION" & > /dev/null
+	fi
 fi
 
 exit 0
